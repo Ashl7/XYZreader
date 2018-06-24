d1cc150155f73dda86be6c1d6c05c9acc2755004
Ashl7 <ashl7developer@gmail.com>
2018-06-23 19:35:26 -0700

    Lottie: start animation when app starts

--- XYZReader/src/main/java/com/example/xyzreader/ui/SplashScreenAnimationActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/SplashScreenAnimationActivity.java
@@ -1,8 +1,16 @@
 package com.example.xyzreader.ui;
 
+import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
+import com.airbnb.lottie.LottieImageAsset;
 import com.example.xyzreader.R;
 
 public class SplashScreenAnimationActivity extends AppCompatActivity {
@@ -11,6 +19,36 @@ public class SplashScreenAnimationActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_animation);
+
+        LottieAnimationView lottieAnimationView = (LottieAnimationView) findViewById(R.id.animation_view);
+        lottieAnimationView.playAnimation();
+
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Intent intent = new Intent(getApplicationContext(), ArticleListActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+
+
+
     }
 
 }
