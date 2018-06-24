26cac122ce2e5d394d1a57f84af25925bb14e108
Ashl7 <ashl7developer@gmail.com>
2018-06-23 18:10:42 -0700

    Lottie: animation works in SplashScreen Activity

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/SplashScreenAnimationActivity.java
@@ -0,0 +1,17 @@
+package com.example.xyzreader.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.example.xyzreader.R;
+
+public class SplashScreenAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_animation);
+    }
+
+}
+
